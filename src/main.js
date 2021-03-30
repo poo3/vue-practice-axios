8f12426e4562e0c239813017e07ca94b80fb46da
バントウフウマ <coolishpoo3@gmail.com>
2021-03-30 19:41:55 +0900

    add axios.interceptors

--- src/main.js
+++ src/main.js
@@ -6,6 +6,26 @@ Vue.config.productionTip = false;
 axios.defaults.baseURL =
   "https://firestore.googleapis.com/v1/projects/http-vuejs-77e10/databases/(default)/documents";
 
+axios.interceptors.request.use(
+  (config) => {
+    console.log("interceptors reqest", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log("interceptors response", response);
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
